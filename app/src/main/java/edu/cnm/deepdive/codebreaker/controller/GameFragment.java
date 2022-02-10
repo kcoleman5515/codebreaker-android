eb7db51c639f577be3b69af6ed14c908ad9a22b1
Kyle Coleman <kcoleman20@cnm.edu>
2022-02-10 11:05:19 -0700

    Started implementation for color-based codes.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
@@ -1,30 +1,36 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import com.google.android.material.snackbar.Snackbar;
 import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.databinding.FragmentGameBinding;
 import edu.cnm.deepdive.codebreaker.model.entity.Guess;
 import edu.cnm.deepdive.codebreaker.viewmodel.GameViewModel;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GameFragment extends Fragment {
 
   private GameViewModel gameViewModel;
   private FragmentGameBinding binding;
+  private Map<Character, Integer> colorValueMap;
+  private Map<Character, String> colorLabelMap;
+  private Character[] poolCharacters;
+  private Spinner[] spinners;
+  private int codeLength;
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,11 +44,12 @@ public class GameFragment extends Fragment {
     binding.submit.setOnClickListener((view) -> {
       //noinspection ConstantConditions
       String text = binding.guess
-         .getText()
-         .toString()
-         .trim();
+          .getText()
+          .toString()
+          .trim();
       gameViewModel.submitGuess(text);
     });
+    setupMaps();
     return binding.getRoot();
   }
 
@@ -80,7 +87,7 @@ public class GameFragment extends Fragment {
   @Override
   public boolean onOptionsItemSelected(@NonNull MenuItem item) {
     boolean handled;
-    if(item.getItemId() == R.id.new_game) {
+    if (item.getItemId() == R.id.new_game) {
       handled = true;
     } else {
       handled = super.onOptionsItemSelected(item);
@@ -92,5 +99,35 @@ public class GameFragment extends Fragment {
   public void onDestroyView() {
     super.onDestroyView();
     binding = null;
+  }
+
+  private void setupMaps() {
+    String colors = getString(R.string.color_chars);
+    char[] colorChars = colors.toCharArray();
+    poolCharacters = colors
+        .chars()
+        .mapToObj((value) -> (char) value)
+        .toArray(Character[]::new);
+    Resources resources = getResources();
+    int[] colorValues = resources.getIntArray(R.array.color_values);
+    String[] colorLabels = resources.getStringArray(R.array.color_labels);
+    colorValueMap = buildValueMap(colorChars, colorValues);
+    colorLabelMap = buildLabelMap(colorChars, colorLabels);
+  }
+
+  private static Map<Character, Integer> buildValueMap(char[] chars, int[] values) {
+    Map<Character, Integer> valueMap = new HashMap<>();
+    for (int i = 0; i < chars.length; i++) {
+      valueMap.put(chars[i], values[i]);
+    }
+    return valueMap;
+  }
+
+  private static Map<Character, String> buildLabelMap(char[] chars, String[] labels) {
+    Map<Character, String> labelMap = new HashMap<>();
+    for (int i = 0; i < chars.length; i++) {
+      labelMap.put(chars[i], labels[i]);
+    }
+    return labelMap;
   }
 }
