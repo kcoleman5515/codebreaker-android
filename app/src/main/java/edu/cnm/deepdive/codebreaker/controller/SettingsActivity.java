758dee7ac0b5dbe2c01a96d1853d3c12a111b7b6
Kyle Coleman <kcoleman20@cnm.edu>
2022-03-30 15:50:30 -0600

    Update of user profile working.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/SettingsActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/SettingsActivity.java
@@ -1,15 +1,16 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
-import android.os.Bundle;
 import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.SwitchPreference;
 import edu.cnm.deepdive.codebreaker.R;
+import edu.cnm.deepdive.codebreaker.model.dto.User;
 import edu.cnm.deepdive.codebreaker.viewmodel.UserViewModel;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -23,14 +24,16 @@ public class SettingsActivity extends AppCompatActivity {
   public static class SettingsFragment extends PreferenceFragmentCompat {
 
     private UserViewModel viewModel;
+    private EditTextPreference displayName;
+    private SwitchPreference incognito;
 
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
       setPreferencesFromResource(R.xml.settings, rootKey);
+      displayName = findPreference(getString(R.string.display_name_pref_key));
+      incognito = findPreference(getString(R.string.incognito_pref_key));
     }
 
-
-    @SuppressWarnings("ConstantConditions")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
       super.onViewCreated(view, savedInstanceState);
@@ -38,12 +41,19 @@ public class SettingsActivity extends AppCompatActivity {
       viewModel
           .getUser()
           .observe(getViewLifecycleOwner(), (user) -> {
-            EditTextPreference displayName = findPreference(
-                getString(R.string.display_name_pref_key));
             displayName.setText(user.getDisplayName());
-            SwitchPreference incognito = findPreference(getString(R.string.incognito_pref_key));
             incognito.setChecked(user.isIncognito());
           });
     }
+
+    @Override
+    public void onDestroyView() {
+      User user = new User();
+      user.setDisplayName(displayName.getText().trim());
+      user.setIncognito(incognito.isChecked());
+      viewModel.updateUser(user);
+      super.onDestroyView();
+    }
   }
+
 }
